65173bc4d07f48a370efecf6bcfb4a60544fc1b9
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-05-18 01:07:12 +0530

    first page

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,8 +1,14 @@
+import 'package:ecommerce_app/home_page.dart';
 import 'package:flutter/material.dart';
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+class FirstPage extends StatefulWidget {
+   FirstPage({super.key});
 
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,11 +34,15 @@ class FirstPage extends StatelessWidget {
             ListTile(
               leading: Icon(Icons.home),
               title: Text('H O M E'),
+              onTap:() {Navigator.pushNamed(context, '/homepage');}
             ),
 
             ListTile(
               leading: Icon(Icons.settings),
               title: Text('S E T T I N G S'),
+              onTap: () {
+                Navigator.pushNamed(context, '/settingspage');
+              },
             )
           ],
         ),
