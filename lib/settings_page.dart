abf2bdfdfa85d9ebf8214fb9efed79acd29f87ae
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-05-18 01:34:28 +0530

    settings page

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -6,9 +6,7 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings Page'),
-      ),
+      body: Center(child: Text('Settings Page')),
     );
   }
 }
