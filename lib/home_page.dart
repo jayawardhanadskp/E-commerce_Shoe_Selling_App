fbf5a6dbd20ae27b0c473a7232ed62aced0fc3b2
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-05-18 01:34:10 +0530

    home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,9 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
+      body: Center(child: Text('Home Page')),
     );
   }
 }
