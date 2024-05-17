30a3151f0f4953b004472ebefb5c3532eae8eb65
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-05-18 01:06:55 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
+
+
+import 'package:ecommerce_app/home_page.dart';
+import 'package:ecommerce_app/settings_page.dart';
 import 'package:flutter/material.dart';
 
 import 'first_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+   MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
-        home: Scaffold(
-          body: FirstPage()
-        ));
+        home: FirstPage(),
+      routes: {
+        '/firstpage': (context) => FirstPage(),
+        '/homepage': (context) => HomePage(),
+        '/settingspage': (context) => SettingsPage()
+      },
+    );
   }
 }
