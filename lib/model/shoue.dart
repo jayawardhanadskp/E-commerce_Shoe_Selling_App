83371b4bd3cd8cb4840ed919afab89e8643e060c
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-05-19 00:20:58 +0530

    shoe

---
+++ lib/model/shoue.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class Shoe {
+  final String name;
+  final String price;
+  final String imagePath;
+  final String description;
+
+  Shoe ({
+    required this.name,
+    required this.price,
+    required this.imagePath,
+    required this.description
+});
+
+}
