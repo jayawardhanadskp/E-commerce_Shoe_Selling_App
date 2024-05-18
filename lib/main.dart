2b752d84385871b92c63566bdf1bacbfcf645178
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-05-19 00:21:04 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-
-
-import 'package:ecommerce_app/home_page.dart';
-import 'package:ecommerce_app/settings_page.dart';
+import 'package:ecommerce_app/model/cart.dart';
 import 'package:flutter/material.dart';
-
-import 'first_page.dart';
+import 'package:provider/provider.dart';
+import 'pages/intro_page.dart';
 
 void main() {
   runApp( MyApp());
@@ -15,15 +12,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        home: FirstPage(),
-      routes: {
-        '/firstpage': (context) => FirstPage(),
-        '/homepage': (context) => HomePage(),
-        '/settingspage': (context) => SettingsPage()
-      },
+    return ChangeNotifierProvider(
+      create: (context) => Cart(),
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: IntroPage(),
+      ),
     );
   }
 }
